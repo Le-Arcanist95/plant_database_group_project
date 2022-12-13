331e39e7018615a38f4b19d580b659f48e9e03d3
noah berlove <noah.berlove@gmail.com>
2022-12-13 15:49:34 -0800

    model changes

--- backend/models/PlantModel.js
+++ backend/models/PlantModel.js
@@ -2,7 +2,15 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema 
 
 const plantsSchema = new Schema ({
-    
+    commonName: {
+        type: String, 
+        required: true,
+    }, 
+    sciName: {
+        type: String,
+        required: true, 
+    }
+
 })
 
 
