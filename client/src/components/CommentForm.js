41da36aa3660c8c036014418ede1cdd99215ab01
noah berlove <noah.berlove@gmail.com>
2022-12-14 11:47:06 -0800

    format change

--- client/src/components/CommentForm.js
+++ client/src/components/CommentForm.js
@@ -2,7 +2,7 @@ import React from "react"
 import { PlantContext } from "./PlantContext"
 
 export default function CommentForm() {
-    const { newComment, setNewComment, addNewComment } = React.useContext(PlantContext)
+    const { setNewComment, addNewComment } = React.useContext(PlantContext)
     function handleChange(event) {
         setNewComment(event.target.value)
     }
@@ -14,6 +14,7 @@ export default function CommentForm() {
             <form>
                 <label for="newComment">Comment: </label>
                 <input type="text" name="newComment" onChange={handleChange}></input>
+                <button>Submit</button>
             </form>
         </>
     )
