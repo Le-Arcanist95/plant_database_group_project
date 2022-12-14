da50c1bcf529b333e4e0ddf1f7cfee7e5dc6d2f2
noah berlove <noah.berlove@gmail.com>
2022-12-13 19:22:41 -0800

    router updates

--- backend/routes/plantRouter.js
+++ backend/routes/plantRouter.js
@@ -2,9 +2,8 @@ const express = require("express");
 const Model = require("backend\models\PlantModel.js");
 const plantRouter = express.Router();
 
-plantRouter.route("/")
-    .get((req, res, next) => {
-        Model.get(req.body, (err, results) => {
+plantRouter.get("/", (req, res, next) => {
+        Model.find((err, results) => {
             if (err) {
                 res.status(400);
                 return next(err);
@@ -12,5 +11,15 @@ plantRouter.route("/")
             res.status(200).send(results);
         });
     });
+
+plantRouter.get("/:plantId", (req, res, next) => {
+    Model.find({_id: req.params._id}, (err, plant) => {
+        if (err) {
+            res.status(400);
+            return next(err)
+        }
+        res.status(200).send(plant)
+    })
+})
 
 module.exports = plantRouter;
