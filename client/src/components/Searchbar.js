f3742a7a9d089a1053d9bbc6876052e571dc755c
noah berlove <noah.berlove@gmail.com>
2022-12-15 20:14:59 -0800

    updates for get onea and search function

--- client/src/components/Searchbar.js
+++ client/src/components/Searchbar.js
@@ -1,20 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import {PlantContext} from "./PlantContext.js"
 import "./styles/searchbar.css";
 
 export default function Searchbar(props) {
-    const [inputValue, setInputValue] = useState("");
+    const [inputValue, setInputValue] = useState({ search : ""});
+    const {setSearchParams, setOtherParams} = useContext(PlantContext)
 
     const handleChange = (e) => {
-        setInputValue(e.target.value);
-    };
+        const {name, value} = e.target
+            setInputValue({
+                [name]: value
+            })
+            
+        } 
+    
 
     // const handleSubmit = () => props.handleSubmit(inputValue);
-    const handleSubmit = () => console.log("queried!");
+    const handleSubmit = () => {
+        console.log("queried!")
+        setSearchParams(inputValue)
+    };
 
     return (
         <>
             <div className="searchbar-wrapper">
-                <input type="search" value={inputValue} onChange={handleChange} />
+                <input type="search" name="search" value={inputValue.search} onChange={handleChange} />
+                <select>
+                    <option></option>
+                </select>
                 <button onClick={handleSubmit}>
                     <i className="bi bi-search"></i>
                     Search
