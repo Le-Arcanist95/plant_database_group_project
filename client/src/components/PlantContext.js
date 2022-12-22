dab258891ddddde633037061ee9bd6caff375730
Levi Arcane <zarcanist95@gmail.com>
2022-12-22 17:44:03 -0500

    Moved reactive components inside of useEffects to clear dependency errors.

--- client/src/components/PlantContext.js
+++ client/src/components/PlantContext.js
@@ -16,7 +16,7 @@ const trefleClient = axios.create({
 function PlantContextProvider(props) {
     // const [authToken, setAuthToken] = useState('');
     const [searchParams, setSearchParams] = useState({})
-    const [searchQuery, setSearchQuery] = useState("?")
+    const [searchQuery, setSearchQuery] = useState("")
     const [searchFilters, setSearchFilters] = useState("")
     const [collection, setCollection] = useState([]);
     const [selectedPlant, setSelectedPlant] = useState();
@@ -27,50 +27,44 @@ function PlantContextProvider(props) {
         // axios.put('');
     }
 
-// Goes through searchParams and sets the search query and string automatically based on the input. 
-    function filterResults() {
-        console.log(searchParams)
-        // if search is undefined, set it to ? (this is for the api call)
-        if (!searchParams.search || searchParams.search === "") {
-            setSearchQuery("?")
-        }
-        // if the search is anything other than the ?  or an empty string, set the search query
-        else if (searchParams.search !== "?" && searchParams.search !== "") {
-            setSearchQuery(`/search?q=${searchParams.search}&`)
-        } 
-        // if the user has selected a minimum or maximum soil humidity, add the range below
-        if (searchParams.minSoilHumidity) {
-            setSearchFilters(`&range[soil_humidity]=${searchParams.minSoilHumidity},${searchParams.maxSoilHumidity}`)
-        }
-        // if the user has selected for only edible plants, add the following filter
-        if (searchParams.isEdible) {
-            setSearchFilters(prev => {
-                return(
-                `${prev}&filter[edible_part]=roots,stem,leaves,flowers,fruits,seeds`
-            )})        
-        }
-        console.log(searchFilters)
-        console.log(searchQuery)
-    }
-     
-    function getSome() {
-       trefleClient.get(`species${searchQuery}${searchFilters}`)
-           .then(res => setCollection(res.data.data))
-           .catch(error => console.log(error));
-    }  
-    
+    // IMPORTANT -- Dependency arrays for both our useEffects were not tracking our functions properly. This was the second time I've pulled down code and encountered the error. These are functions that alter state and should either be tracked or moved inside of the effect. The dependency array is meant to track "reactive" variables and our functions directly interact with state which is exactly that. If we are not tracking the functions that modify state then we are not properly tracking the changes to state.filterResults and getPlants have been moved inside of their respective useEffect. If the functions must live outside of useEffect, they should be wrapped in useCallback. It is best practice to use useCallback when passing functions to child components, as well. - Levi
 
-    console.log(searchParams)
-
-// runs filter search results any time searchparams is updated 
+    // runs filter search results any time searchparams is updated 
     useEffect(() => {
-        filterResults()
+        // Purpose: Goes through searchParams and sets the search query and string automatically based on the input. 
+        const filterResults = () => {
+            // if search is undefined, set it to ? (this is for the api call)
+            if (!searchParams.search || searchParams.search === "") {
+                setSearchQuery("?")
+            }
+            // if the search is anything other than the ?  or an empty string, set the search query
+            else if (searchParams.search !== "?" && searchParams.search !== "") {
+                setSearchQuery(`/search?q=${searchParams.search}&`)
+            } 
+            // if the user has selected a minimum or maximum soil humidity, add the range below
+            if (searchParams.minSoilHumidity) {
+                setSearchFilters(`&range[soil_humidity]=${searchParams.minSoilHumidity},${searchParams.maxSoilHumidity}`)
+            }
+            // if the user has selected for only edible plants, add the following filter
+            if (searchParams.isEdible) {
+                setSearchFilters(prev => {
+                    return(
+                    `${prev}&filter[edible_part]=roots,stem,leaves,flowers,fruits,seeds`
+                )})        
+            }
+        }
+        filterResults();
     }, [searchParams])
     
 // runs getSome call to the api when the filter or query strings are updated. 
     useEffect(() => {
         if (isMounted.current) {
-            getSome()
+            const getPlants = () => {
+                trefleClient.get(`species${searchQuery}${searchFilters}`)
+                    .then(res => setCollection(res.data.data))
+                    .catch(error => console.log(error));
+            }
+            getPlants();
         } else {isMounted.current = true}    
     }, [searchFilters, searchQuery])
 
@@ -86,8 +80,7 @@ function PlantContextProvider(props) {
             selectedPlant: selectedPlant,
             setSelectedPlant: setSelectedPlant,
             searchParams: searchParams,
-            setSearchParams: setSearchParams, 
-            filterResults: filterResults
+            setSearchParams: setSearchParams
         }}>
             {props.children}
         </PlantContext.Provider>
