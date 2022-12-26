a9d50bd3cfa23d9481808e7a92fc8f48b3f992e0
Levi Arcane <zarcanist95@gmail.com>
2022-12-26 18:00:27 -0500

    useEffects created for regex testing

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -26,6 +26,30 @@ const Register = () => {
     const [errMsg, setErrMsg] = useState('');
     const [success, setSuccess] = useState(false);
 
+    useEffect(() => {
+        userRef.current.focus();
+    }, []);
+
+    useEffect(() => {
+        const result = USER_REGEX.test(user);
+        console.log(result);
+        console.log(user);
+        setValidName(result);
+    }, [user]);
+
+    useEffect(() => {
+        const result = PWD_REGEX.test(pwd);
+        console.log(result);
+        console.log(pwd);
+        setValidPwd(result);
+        const match = pwd === matchPwd;
+        setValidMatch(match);
+    }, [pwd, matchPwd]);
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pwd, matchPwd]);
+
     return (
         <div>
             
