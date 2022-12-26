5aaa12d63c616a601ee11102b4615f4dda2a1eb5
Levi Arcane <zarcanist95@gmail.com>
2022-12-26 18:33:47 -0500

    useEffects added for screen focus and error message reset. Error message display added as well.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,17 +1,32 @@
 import React, { useRef, useState, useEffect } from 'react';
 
 const Login = () => {
+    // Refs
     const userRef = useRef();
     const errRef = useRef();
-
+    
+    // State
     const [user, setUser] = useState('');
     const [pwd, setPwd] = useState('');
     const [errMsg, setErrMsg] = useState('');
 
+    // Set focus on user input when page loads
+    useEffect(() => {
+        userRef.current.focus();
+    }, []);
+
+    // Clear error message when user or pwd changes
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pwd]);
+
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <section>
+            {/* Error message display */}
+            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive"> 
+                {errMsg} 
+            </p>
+        </section>
     );
 }
 
