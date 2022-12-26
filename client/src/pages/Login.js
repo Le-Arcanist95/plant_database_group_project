20c8a8904e945f98824818b272ae148564ef6d64
Levi Arcane <zarcanist95@gmail.com>
2022-12-26 18:26:36 -0500

    Added initial state for tracking user, password, and error messages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,6 +4,10 @@ const Login = () => {
     const userRef = useRef();
     const errRef = useRef();
 
+    const [user, setUser] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+
     return (
         <div>
             <h1>Login</h1>
