585f9952aabd42514563ff9f2ef4c842796b5436
Levi Arcane <zarcanist95@gmail.com>
2022-12-26 20:31:19 -0500

    Shifted Providers from App.js into Index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router";
-import { AuthProvider } from "./context/AuthProvider.js";
-import { PlantProvider } from "./context/PlantContext.js";
-import { InputProvider } from "./context/InputContext.js";
 import Homepage from "./pages/Homepage.js";
 import About from "./pages/About.js";
 import Plant from "./pages/Plant.js";
@@ -11,21 +8,11 @@ import User from "./pages/User.js"
 
 export default function App() {
     return (
-        <div>
-            <React.StrictMode>
-                <AuthProvider>
-                    <PlantProvider>
-                        <InputProvider>
-                            <Routes>
-                                <Route path="/" element={<Homepage />} />
-                                <Route path="/about" element={<About />} />
-                                <Route path="/user/:userId" element={<User />} />
-                                <Route path="/plant/:plantId" element={<Plant />} />
-                            </Routes>
-                        </InputProvider>
-                    </PlantProvider>
-                </AuthProvider>
-            </React.StrictMode>
-        </div >
+        <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/user/:userId" element={<User />} />
+            <Route path="/plant/:plantId" element={<Plant />} />
+        </Routes>
     );
 };
