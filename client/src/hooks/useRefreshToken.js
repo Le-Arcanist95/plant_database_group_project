77c0ae5e1e07d344769c1ddd14008573778cc8e9
Levi Arcane <zarcanist95@gmail.com>
2022-12-26 22:55:43 -0500

    Created useRefreshToken custom hook

---
+++ client/src/hooks/useRefreshToken.js
@@ -0,0 +1,21 @@
+import { serverClient } from '../api/axios.js';
+import { useAuth } from './useAuth.js';
+
+const useRefreshToken = () => {
+    const { setAuth } = useAuth();
+
+    const refresh = async () => {
+        try {
+            const { data } = await serverClient.get('/refresh', { withCredentials: true });
+            setAuth(prevAuth => {
+                return { ...prevAuth, accessToken: data.accessToken };
+            });
+            return data.accessToken;
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    return refresh;
+};
+
+export default useRefreshToken;
